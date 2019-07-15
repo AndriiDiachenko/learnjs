6068413257c86659b5077331643c0adf36b328b4
Dandrewd <amdiachenko@gmail.com>
2019-07-15 17:59:11 +0300

    this is something new about Variables in JS

--- html/myjs/myjs.js
+++ html/myjs/myjs.js
@@ -1,1 +1,12 @@
-document.write(' This is my text runs from a seperate  ');
+//
+// document.write(' This is my text runs from a seperate  ');
+//
+//
+// // alert('Putin'); alert('Huilo!');
+// // alert("Pu")
+// // alert('Hu')
+//
+// alert("Hello big world");
+// [1,2].forEach(alert);
+//
+// alert("There will be an error")[1, 2].forEach(alert);
---
+++ html/myjs/variables.js
@@ -0,0 +1,30 @@
+// Variables is here
+
+let message;
+message = "Hello this is my message"; // Storing this string
+
+alert(message);
+
+
+let foo = "One";
+let bar = 'Two';
+
+document.write('this is Foo ' + foo, ' This is Bar ' + bar);
+bar = foo;
+foo = bar;
+document.write("<br>", 'this is Foo ' + foo, ' This is Bar ' + bar);
+
+let $ = 1;
+let _ = 2;
+document.write("<br>", $ + _);
+
+const myname = 'Andrew';
+const myage = 32;
+document.write("<br>", myname, "<br>", myage);
+
+// Ha it not working
+myage = myname;
+document.write("<br>", myage);
+
+
+
